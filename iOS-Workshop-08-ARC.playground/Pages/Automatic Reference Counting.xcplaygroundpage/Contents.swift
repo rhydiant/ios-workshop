3e80bd184ebd8d1c180d8e5cbe9de27dfefb2ca6
Rhydian Thomas <rhydiant@gmail.com>
2017-07-14 07:37:29 +1000

    Additional examples

--- iOS-Workshop-08-ARC.playground/Pages/Automatic Reference Counting.xcplaygroundpage/Contents.swift
+++ iOS-Workshop-08-ARC.playground/Pages/Automatic Reference Counting.xcplaygroundpage/Contents.swift
@@ -5,17 +5,81 @@
 //: * ARC tracks how many properties, constants and variables are currently referring to each class instance
 //: * ARC will not deallocate an instance so long as one active reference exists
 //: * when you assign a class instance to a property, constant or variable, you create a strong reference to it
+// init and deinit
+
+class Animal {
+  init() {
+    print("Initialised a Animal")
+  }
+  deinit {
+    print("Deallocated a Animal")
+  }
+}
+
+//Animal()
+
+//var reference1: Animal? = Animal()
+//reference1 = nil
+
+//var reference1: Animal? = Animal()
+//var reference2: Animal? = reference1
+//reference1 = nil
+//reference2 = nil
 
 /*:
  * callout(strong reference cycle):
  resolving with `weak` and `unowned`
  */
+// it’s possible to write code in which an instance of a class never gets to a point where it has zero strong references
+
+class Teacher {
+  var students = [Student]()
+  
+  deinit {
+    print("Teacher will be deallocated")
+  }
+}
+
+class Student {
+  var teacher: Teacher
+  
+  init(teacher: Teacher) {
+    self.teacher = teacher
+  }
+  
+  deinit {
+    print("Student will be deallocated")
+  }
+}
+
+
+var teacher: Teacher? = Teacher()
+var student: Student? = Student(teacher: teacher!)
+
+//teacher?.students.append(student!) // creates a strong reference cycle!
+
+//teacher = nil
+//student = nil
 
 
 /*:
  * callout(strong reference cycles for closures):
  resolving with closure capture lists
  */
+class Person {
+
+  lazy var printSelfFunction: () -> () = { //[weak self] in
+    print("Person is \(self)")
+  }
+  
+  deinit {
+    print("Person has been deallocated")
+  }
+}
+
+//var person: Person? = Person()
+//person!.printSelfFunction()
+//person = nil
 
 
 //: Up next ...
--- iOS-Workshop-09-Error-Handling.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ iOS-Workshop-09-Error-Handling.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -1,14 +1,32 @@
 //: ### Error Handling
+//: * Swift provides support for throwing, catching, propagating, and manipulating recoverable errors at runtime
+//: * Errors are represented by values of types that conform to the Error protocol
+//: * This empty protocol indicates that a type can be used for error handling
+
 /*:
  * callout(error types and throwing errors):
  using `throw` and the `Error` protocol, error handling by propagation (using `throws`), error handling with do-catch, error handling by converting to optionals (`try?`), disable error propagation (`try!`)
  */
+// define custom error type
+
+
+// throwing errors (throws)
+
+
+// handling errors (do ... catch)
+
+
+// convert errors to optionals (try?)
+
+
+// opting out of error propagation (try!)
 
 
 /*:
  * callout(cleanup actions):
  using `defer`
  */
+// defer statement defers execution until the current scope is exited
 
 
 //: Up next ...
