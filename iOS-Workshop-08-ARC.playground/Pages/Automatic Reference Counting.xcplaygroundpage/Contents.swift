2180a6bd2e942e1a54450b10179a2581a6431bbb
Rhydian Thomas <rhydiant@gmail.com>
2017-07-14 07:39:59 +1000

    Updated example arc

--- iOS-Workshop-08-ARC.playground/Pages/Automatic Reference Counting.xcplaygroundpage/Contents.swift
+++ iOS-Workshop-08-ARC.playground/Pages/Automatic Reference Counting.xcplaygroundpage/Contents.swift
@@ -5,91 +5,82 @@
 //: * ARC tracks how many properties, constants and variables are currently referring to each class instance
 //: * ARC will not deallocate an instance so long as one active reference exists
 //: * when you assign a class instance to a property, constant or variable, you create a strong reference to it
+// init and deinit
+
+class Animal {
+  init() {
+    print("Initialised a Animal")
+  }
+  deinit {
+    print("Deallocated a Animal")
+  }
+}
+
+Animal()
+
+var reference1: Animal? = Animal()
+reference1 = nil
+
+var reference2: Animal? = Animal()
+var reference3: Animal? = reference1
+reference2 = nil
+reference3 = nil
 
 /*:
  * callout(strong reference cycle):
  resolving with `weak` and `unowned`
  */
-// break strong reference cycle with unowned
+// it’s possible to write code in which an instance of a class never gets to a point where it has zero strong references
 
 class Teacher {
   var students = [Student]()
-
+  
   deinit {
     print("Teacher will be deallocated")
   }
 }
 
 class Student {
-  var teacher: Teacher
-
+  var teacher: Teacher?
+  
   init(teacher: Teacher) {
     self.teacher = teacher
   }
-
+  
   deinit {
     print("Student will be deallocated")
   }
 }
 
+
 var teacher: Teacher? = Teacher()
 var student: Student? = Student(teacher: teacher!)
-//teacher?.students.append(student!)
+
+teacher?.students.append(student!) // creates a strong reference cycle!
 
 teacher = nil
 student = nil
 
 
-// break strong reference cycle with weak
-
-class Band {
-  var drummer: Drummer
-
-  init(drummer: Drummer) {
-    self.drummer = drummer
-  }
-
-  deinit {
-    print("band has been deinitialised")
-  }
-}
-
-class Drummer {
-  var band: Band?
-
-  deinit {
-    print("drummer has been deinitialised")
-  }
-}
-
-var drummer: Drummer? = Drummer()
-var band: Band? = Band(drummer: drummer!)
-//drummer?.band = band
-
-band = nil
-drummer = nil
-
 /*:
  * callout(strong reference cycles for closures):
  resolving with closure capture lists
  */
-// break strong reference cycle in closure
-
 class Person {
-
-  lazy var printSelfFunction: () -> () = {
-    // [unowned self] in
-    print("Person is \(self)")
+  
+  lazy var printSelfFunction: () -> () = { [weak self] in
+    print("Person is \(String(describing: self))")
   }
-
+  
   deinit {
-    print("Person has been deinitialised")
+    print("Person has been deallocated")
   }
 }
 
 var person: Person? = Person()
-//person!.printSelfFunction
+person!.printSelfFunction()
 person = nil
+
 
 //: Up next ...
 //: ***
